13acf8745195330e249c27b77176855f4fc663d9
akila-niasa <akilaniasa@gmail.com>
2022-04-29 20:55:57 +0600

    app.put added

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express=require("express")
+const express = require("express")
 require("dotenv").config()
-const app =express()
-const cors=require("cors")
-const port=process.env.PORT||5000
+const app = express()
+const cors = require("cors")
+const port = process.env.PORT || 5000
 
 
 
@@ -16,32 +16,48 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 //   client.close();
 // });
 
-async function run(){
-    try{
+async function run() {
+    try {
         await client.connect();
         const inventoryCollection = client.db("warehouse").collection("inventories");
 
 
-        app.get('/product',async(req,res)=>{
+        app.get('/product', async (req, res) => {
             // const query={}
-            const data=req.query
+            const data = req.query
 
             const products = await inventoryCollection.find(data).toArray();
-            
+
             res.send(products);
         })
 
-        app.get('/product/:id',async(req,res)=>{
-            const id=req.params.id
-            const query={_id:ObjectId(id)}
-            const result=await inventoryCollection.findOne(query)
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const result = await inventoryCollection.findOne(query)
             // console.log(result);
             res.send(result)
         })
 
-       
+        app.put('/product/:id', async (req, res) => {
+            const id = req.params.id
+            // console.log(id);
+            // const query={}
+            const productQuantity = req.body
+            console.log(productQuantity);
+            const objectId = { _id: ObjectId(id) }
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    quantity : productQuantity.quantity
+                },
+            };
+            const result = await inventoryCollection.updateOne(objectId, updateDoc, options)
+            console.log(result);
+            res.send(result)
+        })
     }
-    finally{
+    finally {
 
     }
 }
@@ -50,10 +66,10 @@ run().catch(console.dir)
 app.use(cors())
 app.use(express.json())
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('CURD is running')
 })
 
-app.listen(port,()=>{
-    console.log('CURD is running',port);
+app.listen(port, () => {
+    console.log('CURD is running', port);
 })
